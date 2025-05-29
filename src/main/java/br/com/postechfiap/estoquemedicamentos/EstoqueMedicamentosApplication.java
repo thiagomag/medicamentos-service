631b224b62db3a4e68d806efe85b65814390992f
thiagomagdalena <thiagomagdalena@gmail.com>
2025-05-29 20:07:04 -0300

    Initial commit

---
+++ src/main/java/br/com/postechfiap/estoquemedicamentos/EstoqueMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.estoquemedicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMedicamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoqueMedicamentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/postechfiap/estoquemedicamentos/EstoqueMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.estoquemedicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueMedicamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
