1f96f9722d38d521c94161b7ce7c3208ac1134ee
thiagomagdalena <thiagomagdalena@gmail.com>
2025-07-06 15:26:20 -0300

    correções

--- src/test/java/br/com/postechfiap/medicamentosservice/MedicamentosServiceApplicationTests.java
+++ src/test/java/br/com/postechfiap/medicamentosservice/MedicamentosServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.postechfiap.medicamentosservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MedicamentosServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
