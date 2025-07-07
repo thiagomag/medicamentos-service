d60e9115c03bf70a3f90456e2d473094df63bfa4
thiagomagdalena <thiagomagdalena@gmail.com>
2025-07-06 21:38:17 -0300

    corrigindo envio mensagem

--- src/main/java/br/com/postechfiap/medicamentosservice/infraestructure/producer/NotificacaoProducer.java
+++ src/main/java/br/com/postechfiap/medicamentosservice/infraestructure/producer/NotificacaoProducer.java
@@ -3,11 +3,13 @@ package br.com.postechfiap.medicamentosservice.infraestructure.producer;
 import br.com.postechfiap.medicamentosservice.infraestructure.dto.task.EstoqueAlertaDTO;
 import br.com.postechfiap.medicamentosservice.infraestructure.exceptions.ErroDeProcessamentoAlertaException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class NotificacaoProducer {
 
@@ -25,21 +27,22 @@ public class NotificacaoProducer {
 
     public void enviarNotificacao(EstoqueAlertaDTO estoqueAlertaDTO) {
         try {
+            estoqueAlertaDTO.setTipoNotificacao("MEDICAMENTO_FIM");
             String pedidoJson = objectMapper.writeValueAsString(estoqueAlertaDTO);
-            System.out.println("➡️ Enviando JSON para o Kafka: " + pedidoJson);
+            log.info("➡️ Enviando JSON para o Kafka: " + pedidoJson);
 
             kafkaTemplate.send(topico, estoqueAlertaDTO.getSku(), pedidoJson)
                     .whenComplete((result, ex) -> {
                         if (ex != null) {
-                            System.out.println("alpha ao enviar notificacao para o Kafka: " + ex.getMessage());
+                            log.info("alpha ao enviar notificacao para o Kafka: " + ex.getMessage());
                             throw new ErroDeProcessamentoAlertaException("Erro ao enviar notificacao para o Kafka", ex);
                         }
 
-                        System.out.println("✅ Notificacao enviado com sucesso para o Kafka. Offset: "
+                        log.info("✅ Notificacao enviado com sucesso para o Kafka. Offset: "
                                 + result.getRecordMetadata().offset());
                     });
         } catch (Exception e) {
-            System.err.println("Erro ao serializar a Notificação para JSON: " + e.getMessage());
+            log.error("Erro ao serializar a Notificação para JSON: " + e.getMessage());
             throw new ErroDeProcessamentoAlertaException("Erro ao serializar a Notificacao", e);
         }
     }
